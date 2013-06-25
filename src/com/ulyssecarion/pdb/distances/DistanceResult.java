aaed06c07a0b56181076e0d6d7daa37a0660a171
ulyssecarion <ulyssecarion@gmail.com>
2013-06-25 15:39:05 -0700

    Create a DistanceResult class.
    This will be returned after searching.

---
+++ src/com/ulyssecarion/pdb/distances/DistanceResult.java
@@ -0,0 +1,40 @@
+package com.ulyssecarion.pdb.distances;
+
+import org.biojava.bio.structure.ResidueNumber;
+
+public class DistanceResult {
+	private String pdbID;
+	private int modelNumber;
+	private String chainID;
+	private ResidueNumber residueNumber;
+	private int serialNumber;
+	
+	public DistanceResult(String pdbID, int modelNumber, String chainID,
+			ResidueNumber residueNumber, int serialNumber) {
+		this.pdbID = pdbID;
+		this.modelNumber = modelNumber;
+		this.chainID = chainID;
+		this.residueNumber = residueNumber;
+		this.serialNumber = serialNumber;
+	}
+
+	public String getPdbID() {
+		return pdbID;
+	}
+
+	public int getModelNumber() {
+		return modelNumber;
+	}
+
+	public String getChainID() {
+		return chainID;
+	}
+
+	public ResidueNumber getResidueNumber() {
+		return residueNumber;
+	}
+
+	public int getSerialNumber() {
+		return serialNumber;
+	}
+}
