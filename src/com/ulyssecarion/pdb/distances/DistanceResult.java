ef7fa1e382f9d3f159fc6f635428c24e2e669b3b
ulyssecarion <ulyssecarion@gmail.com>
2013-06-25 16:27:12 -0700

    Create a DistanceResult class
    This time (hopefully) for real; it tracks information about target and
    origin.

---
+++ src/com/ulyssecarion/pdb/distances/DistanceResult.java
@@ -0,0 +1,32 @@
+package com.ulyssecarion.pdb.distances;
+
+public class DistanceResult {
+	private String pdbID; // XXX this is redundant w/ AtomInformation
+	private double distance;
+	private AtomInformation origin;
+	private AtomInformation target;
+
+	public DistanceResult(String pdbID, double distance,
+			AtomInformation origin, AtomInformation target) {
+		this.pdbID = pdbID;
+		this.distance = distance;
+		this.origin = origin;
+		this.target = target;
+	}
+
+	public String getPdbID() {
+		return pdbID;
+	}
+
+	public double getDistance() {
+		return distance;
+	}
+
+	public AtomInformation getOrigin() {
+		return origin;
+	}
+
+	public AtomInformation getTarget() {
+		return target;
+	}
+}
