5a51de7c7f300d0f053c11a8e00c58ae0dc93a02
ulyssecarion <ulyssecarion@gmail.com>
2013-06-26 09:22:26 -0700

    Use a bunch of nested classes to represent the tree of data.

--- src/com/ulyssecarion/pdb/distances/DistanceDataTree.java
+++ src/com/ulyssecarion/pdb/distances/DistanceDataTree.java
@@ -3,37 +3,102 @@ package com.ulyssecarion.pdb.distances;
 import java.util.HashMap;
 import java.util.Map;
 
-// TODO decide whether I should use this or a bunch of subclasses for each level.
+import org.biojava.bio.structure.Element;
+
 public class DistanceDataTree {
-	private DistanceResult result;
-	private Map<String, DistanceDataTree> leaves;
+	public static class OriginGroupTree {
+		private Map<Element, OriginElementTree> children;
 
-	/**
-	 * Constructor for non-leaf nodes.
-	 */
-	public DistanceDataTree() {
-		leaves = new HashMap<>();
+		public OriginGroupTree() {
+			children = new HashMap<>();
+		}
+
+		public void add(Element key, OriginElementTree value) {
+			children.put(key, value);
+		}
+
+		public OriginElementTree get(Element key) {
+			return children.get(key);
+		}
 	}
 
-	/**
-	 * Constructor for leaf nodes.
-	 * 
-	 * @param result
-	 *            information about the result found.
-	 */
-	public DistanceDataTree(DistanceResult result) {
-		this.result = result;
+	public static class OriginElementTree {
+		private Map<String, OriginAtomNameTree> children;
+
+		public OriginElementTree() {
+			children = new HashMap<>();
+		}
+
+		public void add(String key, OriginAtomNameTree value) {
+			children.put(key, value);
+		}
+
+		public OriginAtomNameTree get(String key) {
+			return children.get(key);
+		}
 	}
 
-	public void add(String key, DistanceDataTree val) {
-		leaves.put(key, val);
+	public static class OriginAtomNameTree {
+		private Map<String, TargetGroupTree> children;
+
+		public OriginAtomNameTree() {
+			children = new HashMap<>();
+		}
+
+		public void add(String key, TargetGroupTree value) {
+			children.put(key, value);
+		}
+
+		public TargetGroupTree get(String key) {
+			return children.get(key);
+		}
 	}
 
-	public DistanceDataTree get(String key) {
-		return leaves.get(key);
+	public static class TargetGroupTree {
+		private Map<Element, TargetElementTree> children;
+
+		public TargetGroupTree() {
+			children = new HashMap<>();
+		}
+
+		public void add(Element key, TargetElementTree value) {
+			children.put(key, value);
+		}
+
+		public TargetElementTree get(Element key) {
+			return children.get(key);
+		}
 	}
 
-	public DistanceResult getResult() {
-		return result;
+	public static class TargetElementTree {
+		private Map<String, TargetAtomNameTree> children;
+
+		public TargetElementTree() {
+			children = new HashMap<>();
+		}
+
+		public void add(String key, TargetAtomNameTree value) {
+			children.put(key, value);
+		}
+
+		public TargetAtomNameTree get(String key) {
+			return children.get(key);
+		}
+	}
+
+	public static class TargetAtomNameTree {
+		private Map<String, DistanceResult> children;
+
+		public TargetAtomNameTree() {
+			children = new HashMap<>();
+		}
+
+		public void add(String key, DistanceResult value) {
+			children.put(key, value);
+		}
+
+		public DistanceResult get(String key) {
+			return children.get(key);
+		}
 	}
 }
