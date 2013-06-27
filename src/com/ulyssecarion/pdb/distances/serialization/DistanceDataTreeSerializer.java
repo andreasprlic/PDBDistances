9555d2ebc5743d114d82adcb65782cb935525351
ulyssecarion <ulyssecarion@gmail.com>
2013-06-27 10:54:53 -0700

    Add methods for serializing DistanceDataTrees by their origin group names

--- src/com/ulyssecarion/pdb/distances/serialization/DistanceDataTreeSerializer.java
+++ src/com/ulyssecarion/pdb/distances/serialization/DistanceDataTreeSerializer.java
@@ -6,18 +6,72 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.util.List;
+import java.util.Set;
 
 import com.ulyssecarion.pdb.distances.DistanceDataTree;
+import com.ulyssecarion.pdb.distances.DistanceDataTree.OriginGroupTree;
 
 public class DistanceDataTreeSerializer {
-	private static final String OUTPUT_FILE = "serialized_ddtrees"
+	private static final String DDT_OUTPUT_FILE = "serialized_ddtrees"
+			+ File.separator;
+	private static final String ORIGIN_GROUP_OUTPUT_FILE = "serialized_groups"
 			+ File.separator;
 	private static final String EXTENSION = ".ser";
 
-	public static void serialize(DistanceDataTree dataTree, String name) {
+	public static void serializeByOriginGroups(DistanceDataTree dataTree) {
+		Set<String> originGroups = dataTree.getOriginGroupNames();
+
+		for (String originGroup : originGroups) {
+			serialize(dataTree.get(originGroup), originGroup);
+		}
+	}
+
+	public static OriginGroupTree deserializeOriginGroup(String originGroupName) {
+		try {
+			ObjectInputStream in = new ObjectInputStream(new FileInputStream(
+					ORIGIN_GROUP_OUTPUT_FILE + originGroupName + EXTENSION));
+
+			OriginGroupTree dataTree = (OriginGroupTree) in.readObject();
+
+			in.close();
+			return dataTree;
+		} catch (IOException | ClassNotFoundException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	private static void serialize(List<OriginGroupTree> originGroup, String name) {
 		try {
 			ObjectOutputStream out = new ObjectOutputStream(
-					new FileOutputStream(OUTPUT_FILE + name + EXTENSION));
+					new FileOutputStream(ORIGIN_GROUP_OUTPUT_FILE + name
+							+ EXTENSION));
+
+			// get() returns multiple only if the group name is null;
+			// getting only the first index is a safe process in this case
+			out.writeObject(originGroup.get(0));
+			out.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Serializes an entire DistanceDataTree; you must specify the name of the
+	 * file you want to save to and use this same name to get the
+	 * DistanceDataTree back.
+	 * 
+	 * @param dataTree
+	 *            the DistanceDataTree to save
+	 * @param name
+	 *            the name of the file to save to
+	 * @see #deserializeDataTree(String)
+	 */
+	public static void serializeDataTree(DistanceDataTree dataTree, String name) {
+		try {
+			ObjectOutputStream out = new ObjectOutputStream(
+					new FileOutputStream(DDT_OUTPUT_FILE + name + EXTENSION));
 			out.writeObject(dataTree);
 			out.close();
 		} catch (IOException e) {
@@ -25,10 +79,20 @@ public class DistanceDataTreeSerializer {
 		}
 	}
 
-	public static DistanceDataTree deserialize(String name) {
+	/**
+	 * Deserializes a DistanceDataTree saved at the file whose name is specified
+	 * 
+	 * @param name
+	 *            the location of the DistanceDataTree (this was the argument
+	 *            you passed to
+	 *            {@link #serializeDataTree(DistanceDataTree, String)})
+	 * @return the DistanceDataTree found, or null if an error occurred.
+	 * @see #serializeDataTree(DistanceDataTree, String)
+	 */
+	public static DistanceDataTree deserializeDataTree(String name) {
 		try {
 			ObjectInputStream in = new ObjectInputStream(new FileInputStream(
-					OUTPUT_FILE + name + EXTENSION));
+					DDT_OUTPUT_FILE + name + EXTENSION));
 
 			DistanceDataTree dataTree = (DistanceDataTree) in.readObject();
 
