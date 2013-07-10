18becdc0502e313db67ecb1e5b5fc9f4e6be5081
ulyssecarion <ulyssecarion@gmail.com>
2013-07-10 15:12:21 -0700

    Format a bit, change tests around
    TODO: real tests

--- src/com/ulyssecarion/pdb/distances/DistanceDataTreeDirSearcher.java
+++ src/com/ulyssecarion/pdb/distances/DistanceDataTreeDirSearcher.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import org.biojava.bio.structure.Element;
 
+import com.ulyssecarion.pdb.distances.DistanceQuery.DistanceQueryBuilder;
 import com.ulyssecarion.pdb.distances.serialization.DistanceDataTreeSerializer;
 
 public class DistanceDataTreeDirSearcher {
@@ -17,27 +18,27 @@ public class DistanceDataTreeDirSearcher {
 		File[] originGroups = getCandidates(basefile, q.getOriginGroupName());
 
 		for (File originGroup : originGroups) {
-			//System.out.println(originGroup);
+			// System.out.println(originGroup);
 			File[] originElems = getCandidates(originGroup,
 					q.getOriginElement());
 
 			for (File originElem : originElems) {
-				//System.out.println(originElem);
+				// System.out.println(originElem);
 				File[] originAtoms = getCandidates(originElem,
 						q.getOriginAtomName());
 
 				for (File originAtom : originAtoms) {
-					//System.out.println(originAtom);
+					// System.out.println(originAtom);
 					File[] targetGroups = getCandidates(originAtom,
 							q.getTargetGroupName());
 
 					for (File targetGroup : targetGroups) {
-						//System.out.println(targetGroup);
+						// System.out.println(targetGroup);
 						File[] targetElems = getCandidates(targetGroup,
 								q.getTargetElement());
 
 						for (File targetElem : targetElems) {
-							//System.out.println(targetElem);
+							// System.out.println(targetElem);
 							List<DistanceResult> candidates = getDistanceResults(
 									targetElem, q.getTargetAtomName());
 
@@ -58,14 +59,25 @@ public class DistanceDataTreeDirSearcher {
 	}
 
 	public static void main(String[] args) {
-		DistanceQuery q = new DistanceQuery("BTN", Element.S, null, null, null, null, 0, 8);
+		DistanceQuery q = new DistanceQueryBuilder().originGroup("ZN")
+				.originElement(Element.Zn).targetElement(Element.O).build();
+		System.out.println(q);
 		long start = System.nanoTime();
 		List<DistanceResult> r = search(q);
 		long stop = System.nanoTime();
-		System.out.println((stop - start) / 1_000_000_000.0);
-		
+
 		for (DistanceResult dr : r)
 			System.out.println(dr);
+		
+		List<String> pdbIds = new ArrayList<>();
+		
+		for (DistanceResult dr : r)
+			if (!pdbIds.contains(dr.getPdbID()))
+				pdbIds.add(dr.getPdbID());
+		
+		System.out.println("Results: " + r.size());
+		System.out.println("PDB IDs: " + pdbIds.size());
+		System.out.println("Found in: " + (stop - start) / 1_000_000_000.0);
 	}
 
 	public static File[] getCandidates(File file, Object parameter) {
@@ -88,7 +100,7 @@ public class DistanceDataTreeDirSearcher {
 		File[] candidateFileLocations = getCandidates(file, parameter);
 
 		for (File candidateLocation : candidateFileLocations) {
-			//System.out.println(candidateLocation);
+			// System.out.println(candidateLocation);
 			results.addAll(DistanceDataTreeSerializer
 					.deserializeResults(candidateLocation));
 		}
